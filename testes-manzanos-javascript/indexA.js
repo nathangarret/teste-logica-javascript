ef4815b53da972c0648164f82cbcdaaa9cffbc3c
Nathan V Garrett <ngarrett768@gmail.com>
2023-09-17 15:51:45 -0300

    @garrett testes de lógica

--- testes-manzanos-javascript/indexA.js
+++
@@ -1,8 +0,0 @@
-const entrada = require('prompt-sync')({sigint: true}); // biblioteca do prompt
-
-var grausCelsius = parseFloat(entrada("Temperatura em graus Celsius: "));
-
-var resultadoConversao = (9 * grausCelsius + 160) / 5;
-
-console.log("Temperatura em graus Fahrenheit é = ", grausCelsius);
-console.log("Temperatura em graus Fahrenheit é = ", resultadoConversao);
